897073ad1ec23a18dec6f9d321d58aba1d9e1962
r0ddty <r0d@ukr.net>
2025-03-04 12:32:26 +0200

    fog: fix sdcard path in datapath=
    Signed-off-by: r0ddty <r0d@ukr.net>

--- debian/kernel-info.mk
+++ debian/kernel-info.mk
@@ -11,7 +11,7 @@ VARIANT = android
 KERNEL_BASE_VERSION = 4.19-306
 
 # The kernel cmdline to use
-KERNEL_BOOTIMAGE_CMDLINE = console=tty0 earlycon=msm_geni_serial,0x4a90000 androidboot.hardware=qcom androidboot.console=tty0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 loop.max_part=7 androidboot.selinux=permissive buildvariant=userdebug datapart=/dev/block/mmcblk0p1
+KERNEL_BOOTIMAGE_CMDLINE = console=tty0 earlycon=msm_geni_serial,0x4a90000 androidboot.hardware=qcom androidboot.console=tty0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 loop.max_part=7 androidboot.selinux=permissive buildvariant=userdebug datapart=/dev/mmcblk0p1
 
 # Slug for the device vendor. This is going to be used in the KERNELRELASE
 # and package names.
